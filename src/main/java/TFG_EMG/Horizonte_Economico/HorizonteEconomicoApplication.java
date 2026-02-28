be8b22c387f8c521ee7be93383d725c61cc1df38
Esteban <esteban.martin@campusfp.es>
2026-02-28 12:07:46 +0100

    chore: bootstrap proyecto Horizonte Económico

---
+++ src/main/java/TFG_EMG/Horizonte_Economico/HorizonteEconomicoApplication.java
@@ -0,0 +1,13 @@
+package TFG_EMG.Horizonte_Economico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonteEconomicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizonteEconomicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TFG_EMG/Horizonte_Economico/HorizonteEconomicoApplicationTests.java
@@ -0,0 +1,13 @@
+package TFG_EMG.Horizonte_Economico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorizonteEconomicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
